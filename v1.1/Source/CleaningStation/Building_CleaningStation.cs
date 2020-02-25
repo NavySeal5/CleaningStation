706c0c8df21ec2256f096877e1535b4aec708ee6
NavySeal5 <tobi1.w@gmail.com>
2020-02-26 00:15:45 +0100

    Updated to 1.1

---
+++ v1.1/Source/CleaningStation/Building_CleaningStation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+using HugsLib;
+
+namespace CleaningStation {
+	public class Building_CleaningStation : Building {
+		
+		public CompPowerTrader compPowerTrader;
+		
+		
+		public IEnumerable<IntVec3> cleanableCells{
+			get{
+				return GenRadial.RadialCellsAround(base.Position,CleaningStationSettings.GetCleanRadius(), true);
+			}
+		}
+		
+		
+		public override void SpawnSetup(Map map,bool respawningAfterLoad){
+			base.SpawnSetup(map,respawningAfterLoad);
+			this.compPowerTrader = base.GetComp<CompPowerTrader>();
+			
+			HugsLibController.Instance.DistributedTicker.RegisterTickability(CleanTick,CleaningStationSettings.GetCleanTime(),this);
+		}
+		
+		
+		public void CleanTick(){
+			if(CleaningStationSettings.isDynamicPower()){
+				float powerFactor = -1f*(500f/(500f+(float)cleanableCells.Count()));
+				compPowerTrader.PowerOutput = (float)Math.Round(CleaningStationSettings.GetPowerConsumption() * cleanableCells.Count() * powerFactor);
+				
+			}
+			
+			if(compPowerTrader.PowerOn){				
+				foreach(IntVec3 cleanMe in cleanableCells){
+					FilthMaker.RemoveAllFilth(cleanMe,this.Map);
+				}
+			}		
+		}
+	}
+}
---
+++ v1.1/Source/CleaningStation/PlaceWorker_CleaningStation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace CleaningStation{
+	public class PlaceWorker_CleaningStation : PlaceWorker
+	{
+		public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol,Thing thing=null)
+		{
+			Map currentMap = Find.CurrentMap;
+			GenDraw.DrawRadiusRing(center, CleaningStationSettings.GetCleanRadius());
+		}
+	}
+}
